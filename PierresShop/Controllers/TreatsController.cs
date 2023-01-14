2b37f8a25ed3711d66ed56d01cf65f74644443b6
Yoonis Ali <yoonisali24@Yooniss-Air.home>
2023-01-13 16:19:40 -0800

    add footer and AddFlavor in TreatsController

--- PierresShop/Controllers/TreatsController.cs
+++ PierresShop/Controllers/TreatsController.cs
@@ -25,7 +25,7 @@ namespace PierresShop.Controllers
     {
       return View();
     }
-    
+
     [HttpPost]
     public ActionResult Create(Treat treat)
     {
@@ -42,12 +42,33 @@ namespace PierresShop.Controllers
     }
 
     public ActionResult Details(int id)
-        {
-            Treat thisTreat = _db.Treats
-                .Include(treat => treat.JoinEntities)
-                .ThenInclude(join => join.Flavor)
-                .FirstOrDefault(treat => treat.TreatId == id);
-            return View(thisTreat);
-        }
+    {
+      Treat thisTreat = _db.Treats
+          .Include(treat => treat.JoinEntities)
+          .ThenInclude(join => join.Flavor)
+          .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    public ActionResult AddFlavor(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int flavorId)
+    {
+#nullable enable
+      TreatFlavor? joinEntity = _db.TreatFlavors.FirstOrDefault(join => (join.FlavorId == flavorId && join.TreatId == treat.TreatId));
+      #nullable disable
+      if (joinEntity == null && flavorId != 0)
+      {
+        _db.TreatFlavors.Add(new TreatFlavor() { FlavorId = flavorId, TreatId = treat.TreatId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = treat.TreatId });
+    }
   }
 }
